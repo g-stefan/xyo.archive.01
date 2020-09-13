8e7184306f84bd5d4e16c1947f2cd59b5581a482
Grigore Stefan <g_stefan@yahoo.com>
2020-09-14 00:03:28 +0300

    update

--- port/build.platform.local-release.cmd
+++ port/build.platform.local-release.cmd
@@ -3,7 +3,7 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> platorm local-release xyo
+echo -^> platform local-release xyo
 
 goto cmdXDefined
 :cmdX

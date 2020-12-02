a658c9a531c5837bd7c1802ff346ff6c4da080be
Grigore Stefan <g_stefan@yahoo.com>
2020-12-02 11:33:03 +0200

    update

--- port/build.platform.git-release.cmd
+++ port/build.platform.git-release.cmd
@@ -7,10 +7,10 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 echo -^> git-release xyo
 
-if not exist release\ echo Error - no release && exit 1
+if not exist release\ echo Error - no release & exit 1
 
 set PROJECT=xyo
-if not exist source\%PROJECT%.version.ini echo Error - no version && exit 1
+if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
 FOR /F "tokens=* USEBACKQ" %%F IN (`xyo-version --no-bump --get "--version-file=source\%PROJECT%.version.ini" %PROJECT%`) DO (
 	SET VERSION=%%F
 )

e81ba74d4b4e4d60c459c0f1699b0be3115deee6
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:18:07 +0200

    Update

--- build/platform/platform.github-release.cmd
+++ build/platform/platform.github-release.cmd
@@ -7,6 +7,8 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 echo - %BUILD_PROJECT% ^> platform github-release
 
+if "%GITHUB_PROJECT%" == "" set GITHUB_PROJECT=%BUILD_PROJECT%
+
 if not exist release\ echo Error - no release & exit 1
 
 set PROJECT=%BUILD_PROJECT%
@@ -15,18 +17,18 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`xyo-version --no-bump --get "--version-file=
 	SET VERSION=%%F
 )
 
-echo -^> release %PROJECT% v%VERSION%
+echo -^> release %GITHUB_PROJECT% v%VERSION%
 
 git pull --tags origin main
 git rev-parse --quiet "v%VERSION%" 1>NUL 2>NUL
 if not errorlevel 1 goto tagExists
 git tag -a v%VERSION% -m "v%VERSION%"
 git push --tags
-echo Create release %PROJECT% v%VERSION%
-github-release release --repo %PROJECT% --tag v%VERSION% --name "v%VERSION%" --description "Release"
+echo Create release %GITHUB_PROJECT% v%VERSION%
+github-release release --repo %GITHUB_PROJECT% --tag v%VERSION% --name "v%VERSION%" --description "Release"
 pushd release
-for /r %%i in (%PROJECT%-%VERSION%*.7z) do echo Upload %%~nxi & github-release upload --repo %PROJECT% --tag v%VERSION% --name "%%~nxi" --file "%%i"
-for /r %%i in (%PROJECT%-%VERSION%*.csv) do echo Upload %%~nxi & github-release upload --repo %PROJECT% --tag v%VERSION% --name "%%~nxi" --file "%%i"
+for /r %%i in (%PROJECT%-%VERSION%*.7z) do echo Upload %%~nxi & github-release upload --repo %GITHUB_PROJECT% --tag v%VERSION% --name "%%~nxi" --file "%%i"
+for /r %%i in (%PROJECT%-%VERSION%*.csv) do echo Upload %%~nxi & github-release upload --repo %GITHUB_PROJECT% --tag v%VERSION% --name "%%~nxi" --file "%%i"
 popd
 
 goto :eof
